d882a65061101837436ee44036f80a62680b2614
Charles Feduke <charles.feduke@gmail.com>
2011-02-19 20:33:36 -0500

    Initial commit

---
+++ ImageFunAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ImageFunAppDelegate.h
+//  ImageFun
+//
+//  Created by Charles Feduke on 2/19/11.
+//  Copyright 2011 Mobile Posse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ImageFunAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ImageFunAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  ImageFunAppDelegate.m
+//  ImageFun
+//
+//  Created by Charles Feduke on 2/19/11.
+//  Copyright 2011 Mobile Posse. All rights reserved.
+//
+
+#import "ImageFunAppDelegate.h"
+
+@implementation ImageFunAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
